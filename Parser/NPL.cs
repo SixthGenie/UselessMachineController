29b198368aa2add3c800cd13ad86c863e50e30d6
SixthGenie <SixthGenie@users.noreply.github.com>
2015-01-27 08:34:22 +0100

    Update NPL.cs

--- Parser/NPL.cs
+++ Parser/NPL.cs
@@ -1,4 +1,17 @@
-﻿using System;
+/*
+COPYRIGHT NOTICE for the  UseLessMachineController
+
+Copyright (c) 2014-2015 Kjetil Næss.
+
+This program is free software for personal use only. You may modify and/or distribute as you like as long as this message
+is included in the distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY whatsoever. It if works it works, if it doesn't, you have the source to fix it
+(remember to let me know so that I can fix it on my end).
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
